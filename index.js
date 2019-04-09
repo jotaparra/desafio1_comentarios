7ba3d8d7aaa14b0adc50bb4ee905b1a6df4e7286
Jennifer Parra <jparra.biblio@gmail.com>
2019-04-09 01:11:27 -0400

    desafío comentarios - listo

--- index.js
+++ index.js
@@ -10,19 +10,13 @@ var db = firebase.firestore();
 //AGREGAR DATOS. Código de firestore
 //Creamos una función 'guardar' para que cada vez que apretamos el botón, guarde la información y no lo haga automático como lo estaba haciendo cada vez que recargabamos la página
 function guardar(){
-    let nombre = document.getElementById('nombre').value; 
-    let apellido = document.getElementById('apellido').value; 
-    let fecha = document.getElementById('fecha').value; 
+    let comment = document.getElementById('comment').value; 
     db.collection("users").add({   //add: agrega un ID automático a nuestro 'users'
-        first: nombre,
-        last: apellido,
-        born: fecha
+        comment: comment,
     })
     .then(function(docRef) {     //en esta parte se ejecuta la parte del exito
         console.log("Document written with ID: ", docRef.id);
-        document.getElementById('nombre').value = '';
-        document.getElementById('apellido').value = '';
-        document.getElementById('fecha').value = '';
+        document.getElementById('comment').value = '';
     })
     .catch(function(error) {
         console.error("Error adding document: ", error);
@@ -35,13 +29,10 @@ db.collection("users").onSnapshot((querySnapshot) => {  //Para obtener actualiza
     //vamos a limpiar la tabla para que empiece desde 0
     tabla.innerHTML = '';
     querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
+        console.log(`${doc.id} => ${doc.data().comment}`);
         //pintar datos
         tabla.innerHTML += `<tr>
-        <th scope="row">${doc.id}1</th>
-        <td>${doc.data().first}</td>
-        <td>${doc.data().last}</td>
-        <td>${doc.data().born}</td>
+        <td>${doc.data().comment}</td>
         <td><button class='btn btn-danger' onclick='eliminar("${doc.id}")'>Eliminar</button></td>
         </tr>`
     });
